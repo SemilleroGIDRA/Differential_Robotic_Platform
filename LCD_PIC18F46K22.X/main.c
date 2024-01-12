8924eb01ac933128669ed6a0546d8ded76b72b58
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-01-12 09:48:56 -0500

    Create the project and make the fuses library to set the bits of the microcontroller.

---
+++ LCD_PIC18F46K22.X/main.c
@@ -0,0 +1,19 @@
+/*
+ * File:   main.c
+ * Author: Alejo
+ *
+ * Created on January 12, 2024, 9:16 AM
+ */
+
+
+#include <xc.h>
+#include "Fuses_Set.h"
+
+
+void main(void) {
+    
+    while (1){
+        
+    }
+    
+}
