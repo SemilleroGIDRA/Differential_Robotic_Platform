e9eb4b3eb1252b14060e8d1af8982b1fbbe43627
Daniel ALejandro Reyes León <alle262001@gmail.com>
2024-02-14 18:19:08 -0500

    Create platform template and add fuses configuration

---
+++ Platform_Project.X/main.c
@@ -0,0 +1,26 @@
+/*
+ * File:   main.c
+ * Author: Alejo
+ *
+ * Created on February 14, 2024, 6:11 PM
+ */
+
+
+#include <xc.h>
+#include <stdint.h>
+#include "Fuses_Configuration.h"
+
+
+//Main function.
+void main(void) {
+    
+    //Call functions. 
+    
+    //Infinite Loop. 
+    while(1){
+        
+        
+        
+    }
+   
+}
